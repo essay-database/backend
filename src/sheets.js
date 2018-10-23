53b2643b6477542b8df68d4a5ea5ceb0f344e07d
Samuel k. Mbaabu <samuel.karani@berkeley.edu>
2018-10-23 14:43:58 -0700

    sheets now working

--- src/sheets.js
+++ src/sheets.js
@@ -89,7 +89,6 @@ function getEssay(params) {
 
 }
 
-
 function getEssays(auth) {
     const sheets = google.sheets({
         version: 'v4',
@@ -102,8 +101,6 @@ function getEssays(auth) {
         if (err) return console.log('The API returned an error: ' + err);
         const rows = res.data.values;
         if (rows.length) {
-            console.log('Name, Major:');
-            // Print columns A and E, which correspond to indices 0 and 4.
             rows.map((row) => {
                 console.log(row);
             });
