324defe6d44a4758aa9d579b8f1ab03248612cf2
Samuel k. Mbaabu <samuel.karani@berkeley.edu>
2018-11-05 22:53:04 -0800

    added cors

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
+const cors = require("cors");
 const express = require("express");
 const essaysRouter = require("./src/routes");
 const { createError } = require("./src/api");
 
 const app = express();
+app.use(cors());
 app.use(express.json());
 app.use("/essays", essaysRouter);
 
